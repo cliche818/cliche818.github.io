8264cbd9d19ea5375fe74225a08514aa88a6952f
jeffli <jeff.li@retailcommon.com>
2016-01-11 10:07:17 -0500

    fixed bug with the way king.protoype was being set, still need to fix unit tests

--- js/main/king.js
+++ js/main/king.js
@@ -4,12 +4,10 @@ function King() {
 
 King.prototype = new ChessPiece();
 
-King.prototype = {
-    validMove : function(destinationX, destinationY) {
+King.prototype.validMove = function(destinationX, destinationY) {
         var xDifference = Math.abs(this.x - destinationX);
         var yDifference = Math.abs(this.y - destinationY);
         console.log( ChessPiece.prototype.validMove.call(this, destinationX, destinationY));
-        //var baseRuleValid = ChessPiece.prototype.validMove.call(this,destinationX, destinationY );
+        var baseRuleValid = ChessPiece.prototype.validMove.call(this,destinationX, destinationY );
         return baseRuleValid ||( xDifference === 1 && destinationY === this.y) || (yDifference === 1 && destinationX === this.x) || (xDifference === 1 && yDifference === 1);
-    }
-};
+    };
