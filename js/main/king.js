05b13863bab781a11b881a620f571c7b598fba3c
Cliche818 <jeff.chun.yinli@gmail.com>
2016-01-13 21:51:28 -0500

    fixed failing test

--- js/main/king.js
+++ js/main/king.js
@@ -7,7 +7,6 @@ King.prototype = new ChessPiece();
 King.prototype.validMove = function(destinationX, destinationY) {
         var xDifference = Math.abs(this.x - destinationX);
         var yDifference = Math.abs(this.y - destinationY);
-        console.log( ChessPiece.prototype.validMove.call(this, destinationX, destinationY));
         var baseRuleValid = ChessPiece.prototype.validMove.call(this,destinationX, destinationY );
-        return baseRuleValid ||( xDifference === 1 && destinationY === this.y) || (yDifference === 1 && destinationX === this.x) || (xDifference === 1 && yDifference === 1);
+        return baseRuleValid && ( ( xDifference === 1 && destinationY === this.y) || (yDifference === 1 && destinationX === this.x) || (xDifference === 1 && yDifference === 1) );
     };
