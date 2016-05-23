a038ed39092dee897c680489d72bbb014f013c8e
jeffli <jeff.li@retailcommon.com>
2016-05-22 22:31:12 -0400

    refactored queenSpec as tests are in coordinate now and renamed path to _path to indicate internal function

--- test/queenSpec.js
+++ test/queenSpec.js
@@ -10,12 +10,22 @@ describe('Queen', function () {
       queen.coordinate = new Coordinate(4,4);
     });
 
-    it('is a valid move going horizontally', function() {
+    it('should return true for a valid move going horizontally', function() {
       expect(queen.validMove(7,4)).toBe(true);
+      expect(queen.validMove(1,4)).toBe(true);
     });
 
-    it('is a valid move going verically', function() {
+    it('should return true for a valid move going verically', function() {
       expect(queen.validMove(4,5)).toBe(true);
+      expect(queen.validMove(4,1)).toBe(true);
+      expect(queen.validMove(4,7)).toBe(true);
+    });
+
+    it('should return true for a valid move going diagonal', function(){
+      expect(queen.validMove(1,1)).toBe(true);
+      expect(queen.validMove(7,1)).toBe(true);
+      expect(queen.validMove(1,7)).toBe(true);
+      expect(queen.validMove(7,7)).toBe(true);
     });
 
     it('is a not valid move', function() {
@@ -27,49 +37,5 @@ describe('Queen', function () {
       expect(queen.validMove(3,1)).toBe(false);
     });
 
-  });
-
-
-  describe('path', function () {
-    beforeEach(function () {
-      queen.coordinate = new Coordinate(4,4);
-    });
-
-    it('going vertically, provide a list of coordinates of steps to its destination', function () {
-      expect(queen.path(7, 4)).toEqual([[5, 4], [6, 4], [7, 4]]);
-      expect(queen.path(1, 4)).toEqual([[3, 4], [2, 4], [1, 4]]);
-    });
-
-    it('going horizontally, provide a list of coordinates of steps to its destination', function () {
-      expect(queen.path(4, 7)).toEqual([[4, 5], [4, 6], [4, 7]]);
-      expect(queen.path(4, 1)).toEqual([[4, 3], [4, 2], [4, 1]]);
-    });
-
-    describe('going diagonally, provide a list of coordinates of steps to its destination', function () {
-      it('bottom left', function () {
-        expect(queen.path(1, 1)).toEqual([[3, 3], [2, 2], [1, 1]]);
-      });
-
-      it('bottom right', function() {
-        expect(queen.path(7, 1)).toEqual([[5, 3], [6, 2], [7, 1]]);
-      });
-
-      it('top left', function() {
-        expect(queen.path(1, 7)).toEqual([[3, 5], [2, 6], [1, 7]]);
-      });
-
-      it('top right', function() {
-        expect(queen.path(7, 7)).toEqual([[5, 5], [6, 6], [7, 7]]);
-      });
-    });
-
-    it('return empty array if its an impossible path', function() {
-      expect(queen.path(5,6)).toEqual([]);
-      expect(queen.path(5,7)).toEqual([]);
-      expect(queen.path(7,5)).toEqual([]);
-      expect(queen.path(7,6)).toEqual([]);
-      expect(queen.path(1,3)).toEqual([]);
-      expect(queen.path(3,1)).toEqual([]);
-    });
   });
 });
