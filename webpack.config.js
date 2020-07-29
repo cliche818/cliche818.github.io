13a35262da9ba704415e74bf7e109442f7c0d208
cliche818 <jeffli@Jeffs-MacBook-Pro.local>
2020-07-29 13:42:31 -0400

    added webpack for creating a static js file that jekyll can use

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./webpack/entry.js",
+    output: {
+      // we’re going to put the generated file in the assets folder so jekyll will grab it.
+      // if using GitHub Pages, use the following:
+      // path: "assets/javascripts"
+      path: __dirname + "/assets",
+      filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+  };
